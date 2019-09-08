75e6219636dfd41042c45a82b855233d52c4055e
Ivan <razgylin@gmail.com>
2019-09-08 20:43:39 +0300

    added test database

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+db.host=localhost:3306
+db.user=root
+db.pass=root
 1. выполнить скрипт create-tables.sql
 2. mvn spring-boot:run
--- create_tables.sql
+++ create_tables.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE database0;
+CREATE DATABASE IF NOT EXISTS database0;
 USE database0;
 
 DROP TABLE IF EXISTS users;
@@ -22,7 +22,7 @@ CREATE TABLE payments (
   PRIMARY KEY (id)
 );
 
-CREATE DATABASE database1;
+CREATE DATABASE IF NOT EXISTS database1;
 USE database1;
 
 DROP TABLE IF EXISTS payments;
@@ -34,8 +34,56 @@ CREATE TABLE payments (
   PRIMARY KEY (id)
 );
 
-CREATE DATABASE database2;
+CREATE DATABASE IF NOT EXISTS database2;
 USE database2;
+
+DROP TABLE IF EXISTS payments;
+CREATE TABLE payments (
+  id bigint NOT NULL,
+  sender_user_id bigint NOT NULL,
+  recipient_user_id bigint NOT NULL,
+  amount decimal(19,4) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE DATABASE IF NOT EXISTS database_test0;
+USE database_test0;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+   id bigint NOT NULL,
+   name varchar(255) NOT NULL,
+   PRIMARY KEY (id)
+);
+INSERT INTO users (id, name)
+VALUES (1, 'Alex'),
+       (2, 'Poll'),
+       (3, 'Martin'),
+       (4, 'Tom');
+
+DROP TABLE IF EXISTS payments;
+CREATE TABLE payments (
+  id bigint NOT NULL,
+  sender_user_id bigint NOT NULL,
+  recipient_user_id bigint NOT NULL,
+  amount decimal(19,4) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE DATABASE IF NOT EXISTS database_test1;
+USE database_test1;
+
+DROP TABLE IF EXISTS payments;
+CREATE TABLE payments (
+  id bigint NOT NULL,
+  sender_user_id bigint NOT NULL,
+  recipient_user_id bigint NOT NULL,
+  amount decimal(19,4) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE DATABASE IF NOT EXISTS database_test2;
+USE database_test2;
 
 DROP TABLE IF EXISTS payments;
 CREATE TABLE payments (
